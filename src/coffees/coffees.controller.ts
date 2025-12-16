e1d89670ffda94fe56862de7ef111a11e661bc82
Charlie Li <lixuan3901@gmail.com>
2025-12-17 02:13:53 +0800

    p11 customer status code

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,10 +1,27 @@
-import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  Query,
+  Res,
+} from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
   @Get('')
   findAll() {
     return 'This action returns all coffees';
+  }
+
+  @Get('/list-res')
+  findAll2(@Res() response) {
+    // 使用response对象自定义响应
+    // 注意：使用response对象后，Nest将不会再处理响应, 不建议使用
+    response.status(200).send('This action returns all coffees');
   }
 
   // 设置url的子路径
@@ -41,6 +58,7 @@ export class CoffeesController {
 
   // Body Params
   @Post()
+  @HttpCode(HttpStatus.GONE)
   create(@Body() body) {
     return body;
   }

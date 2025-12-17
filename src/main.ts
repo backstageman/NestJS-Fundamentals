005bc382c2e64118fba18f290853360c4850a55f
Charlie Li <lixuan3901@gmail.com>
2025-12-18 01:58:41 +0800

    p18 config dto pipe

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,12 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // 去除DTO中没有的属性
+      forbidNonWhitelisted: true, // 有非DTO中的属性时抛出错误
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();

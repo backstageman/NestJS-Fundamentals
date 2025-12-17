231b5b0a3d6c883e526b9581a3d9a1291d0dea9e
Charlie Li <lixuan3901@gmail.com>
2025-12-18 04:47:14 +0800

    p22 & p23 config docker and typeorm module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CoffeesController } from './coffees/coffees.controller';
-import { CoffeesService } from './coffees/coffees.service';
 import { CoffeesModule } from './coffees/coffees.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [
+    CoffeesModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'pass123',
+      database: 'postgres',
+      autoLoadEntities: true, // 自动加载实体
+      synchronize: true, // 在开发环境中使用，自动同步实体到数据库，生产环境中应禁用
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

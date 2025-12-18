7d6033e8c0a3b6e04da667c00ac50135534d536f
Charlie Li <lixuan3901@gmail.com>
2025-12-19 02:48:18 +0800

    p27 Retrieve  entities with their relations

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -26,12 +26,17 @@ export class CoffeesService {
   ) {}
 
   findAll() {
-    return this.coffeeRepository.find();
+    return this.coffeeRepository.find({
+      relations: ['flavors'],
+    });
   }
 
   async findOne(id: string) {
     // throw 'a random error';
-    const coffee = await this.coffeeRepository.findOne({ where: { id: +id } });
+    const coffee = await this.coffeeRepository.findOne({
+      where: { id: +id },
+      relations: ['flavors'],
+    });
     if (!coffee) {
       /*  throw new HttpException(`Coffee #${id} not found`, HttpStatus.NOT_FOUND); */
       throw new NotFoundException(`Coffee #${id} not found`);

bd95e1752137a62bd5150504b84b221ad4e63fbe
Charlie Li <lixuan3901@gmail.com>
2025-12-19 04:34:54 +0800

    p29 adding pagination

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -14,14 +14,15 @@ import {
 import { CoffeesService } from './coffees.service';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
+import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto/pagination-query.dto';
 
 @Controller('coffees')
 export class CoffeesController {
   constructor(private readonly coffeesService: CoffeesService) {}
 
   @Get()
-  findAll() {
-    return this.coffeesService.findAll();
+  findAll(@Query() paginationQuery: PaginationQueryDto) {
+    return this.coffeesService.findAll(paginationQuery);
   }
 
   @Get('/list-res')
--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,9 @@ async function bootstrap() {
       whitelist: true, // 去除DTO中没有的属性
       forbidNonWhitelisted: true, // 有非DTO中的属性时抛出错误
       transform: true, // 自动转换参数类型
+      transformOptions: {
+        enableImplicitConversion: true, // 启用隐式类型转换
+      },
     }),
   );
   await app.listen(3000);

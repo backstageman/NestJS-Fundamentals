f7130a654352f6e7699f7752a300212cf770eea6
Charlie Li <lixuan3901@gmail.com>
2025-12-18 15:50:01 +0800

    p24  Associate entity and table

--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,6 +1,16 @@
+import { Column, Entity, PrimaryColumn } from 'typeorm';
+
+@Entity() // sql用小写命名表coffee
 export class Coffee {
+  @PrimaryColumn()
   id: number;
+
+  @Column()
   name: string;
+
+  @Column()
   brand: string;
+
+  @Column('json', { nullable: true })
   flavors: string[];
 }
